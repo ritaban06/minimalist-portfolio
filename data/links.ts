88e661bee2ddb25aa2d15613d8e452baa7bf77e4
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-12-23 11:01:29 +0530

    Remove HomeBlog component and related blog links
    Deleted the HomeBlog.tsx component and removed the commented-out blog links from links.ts. This cleans up unused code and references to blog posts that are no longer needed.

--- data/links.ts
+++ data/links.ts
@@ -9,13 +9,6 @@ export const links = {
       extreviewbot: 'https://github.com/yourusername/extreviewbot',
     },
   },
-  // blog: {
-  //   soldJobBoard:
-  //     'https://www.indiehackers.com/post/how-i-sold-my-niche-job-board-for-13-500-046f1aa443',
-  //   animatedCursor: 'https://dev.to/andrewchmr/awesome-animated-cursor-with-react-hooks-5ec3',
-  //   reactTimeInput: 'https://dev.to/andrewchmr/react-hh-mm-ss-time-input-cfl',
-  //   sunburstChart: 'https://dev.to/andrewchmr/react-d3-sunburst-chart-3cpd',
-  // },
   social: {
     x: 'https://x.com/RitabanGho95222',
     linkedin: 'https://www.linkedin.com/in/ritaban-ghosh-739428315/',
