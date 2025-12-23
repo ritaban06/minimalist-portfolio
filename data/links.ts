174d0dc24522392bda3f0233bfb42b943eeb744d
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-12-23 10:08:53 +0530

    Comment out blog links in links.ts
    The blog section in the links.ts file has been commented out, possibly to temporarily disable or hide these links from use in the application.

--- data/links.ts
+++ data/links.ts
@@ -8,13 +8,13 @@ export const links = {
     ingastro:
       'https://www.indiehackers.com/post/how-i-sold-my-niche-job-board-for-13-500-046f1aa443',
   },
-  blog: {
-    soldJobBoard:
-      'https://www.indiehackers.com/post/how-i-sold-my-niche-job-board-for-13-500-046f1aa443',
-    animatedCursor: 'https://dev.to/andrewchmr/awesome-animated-cursor-with-react-hooks-5ec3',
-    reactTimeInput: 'https://dev.to/andrewchmr/react-hh-mm-ss-time-input-cfl',
-    sunburstChart: 'https://dev.to/andrewchmr/react-d3-sunburst-chart-3cpd',
-  },
+  // blog: {
+  //   soldJobBoard:
+  //     'https://www.indiehackers.com/post/how-i-sold-my-niche-job-board-for-13-500-046f1aa443',
+  //   animatedCursor: 'https://dev.to/andrewchmr/awesome-animated-cursor-with-react-hooks-5ec3',
+  //   reactTimeInput: 'https://dev.to/andrewchmr/react-hh-mm-ss-time-input-cfl',
+  //   sunburstChart: 'https://dev.to/andrewchmr/react-d3-sunburst-chart-3cpd',
+  // },
   social: {
     x: 'https://x.com/RitabanGho95222',
     linkedin: 'https://www.linkedin.com/in/ritaban-ghosh-739428315/',
